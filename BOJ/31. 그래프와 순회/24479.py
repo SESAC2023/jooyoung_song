bb45e0bc2b4769da089c560f073ca66d2b50957f
Song <64202860+Song-Joo-Young@users.noreply.github.com>
2023-06-22 22:48:31 +0900

    Create 24479.py

---
+++ BOJ/31. 그래프와 순회/24479.py
@@ -0,0 +1,61 @@
+"""
+import sys
+sys.setrecursionlimit(int(1e6))
+input = sys.stdin.readline
+
+n, m, r = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+visited = [False]*(n+1)
+answer = [0] * (n+1)
+
+for i in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+cnt = 1
+
+def dfs(x):
+    global cnt
+    visited[x] = True
+    answer[x] = cnt
+    cnt+=1
+    for y in graph[x]:
+        if not visited[y]:
+            dfs(y)
+
+for i in range(1, n+1):
+    graph[i].sort()
+
+dfs(r)
+for i in range(1, n+1):
+    print(answer[i])
+"""
+
+# 틀린 코드
+import sys
+sys.setrecursionlimit(int(1e6))
+input = sys.stdin.readline
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v)
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+  
+N, M, R = map(int, input().split(' '))
+graph=[[] for _ in range(N+1)]
+visited = [False]*(N+1)
+
+for i in range(1, M+1):
+    u, v = map(int, input().split(' '))
+    graph[u].append(v)
+    graph[v].append(u)
+
+for adjacent_list in graph:
+    adjacent_list.sort()
+
+dfs(graph, R, visited)
+print("0")
