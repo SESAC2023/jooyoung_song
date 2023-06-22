3868a6a862b4c64cc44ee7ee79f963678c90d45c
ndb7967 <122754860+ndb7967@users.noreply.github.com>
2023-06-22 22:52:54 +0900

    Update 24479.py

--- BOJ/31. 그래프와 순회/24479.py
+++ BOJ/31. 그래프와 순회/24479.py
@@ -33,6 +33,7 @@ for i in range(1, n+1):
     print(answer[i])
 """
 
+"""
 # 틀린 코드
 import sys
 sys.setrecursionlimit(int(1e6))
@@ -59,3 +60,37 @@ for adjacent_list in graph:
 
 dfs(graph, R, visited)
 print("0")
+"""
+
+# 맞은 코드
+import sys
+sys.setrecursionlimit(int(1e6))
+input = sys.stdin.readline
+
+order = 1
+def dfs(graph, v, visited):
+    global order
+    visited[v] = True
+    answer[v] = order
+    order += 1
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+  
+N, M, R = map(int, input().split(' '))
+graph=[[] for _ in range(N+1)]
+visited = [False]*(N+1)
+answer = [0] * (N + 1)
+
+for i in range(1, M+1):
+    u, v = map(int, input().split(' '))
+    graph[u].append(v)
+    graph[v].append(u)
+
+for adjacent_list in graph:
+    adjacent_list.sort()
+
+dfs(graph, R, visited)
+
+for i in range(1, N + 1):
+    print(answer[i])
