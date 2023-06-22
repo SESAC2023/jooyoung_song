6ad771036674acd0a884200928c00340cc11b750
Song <64202860+Song-Joo-Young@users.noreply.github.com>
2023-06-22 22:47:46 +0900

    Update 1316.py

--- BOJ/6. 심화 1/1316.py
+++ BOJ/6. 심화 1/1316.py
@@ -12,3 +12,26 @@ for _ in range(N):
         count += 1
 
 print(count)
+
+
+"""
+def group(string):
+    chars = [False] * 26
+    # 각 알파벳에 대한 방문 여부
+    for i in range(len(string):
+        x = ord(string[i]) - 97
+        # 방문한 적 없는 알파벳이라면
+        if not chars[x]:
+            chars[x] = True
+        # 방문한 적 있는 알파벳이라면
+        else:
+            if string[i] != string[i-1]:
+                return False
+    return True
+
+N = int(input())
+cnt = 0
+for i in range(N):
+    string = input()
+    if group(string): cnt += 1
+print(cnt)
