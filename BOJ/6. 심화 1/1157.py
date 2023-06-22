d35409e36c5c6df33288d7234d5246481f626a06
Song <64202860+Song-Joo-Young@users.noreply.github.com>
2023-06-22 20:46:14 +0900

    Update 1157.py

--- BOJ/6. 심화 1/1157.py
+++ BOJ/6. 심화 1/1157.py
@@ -11,3 +11,41 @@ if count_list.count(max(count_list)) > 1:
 else:
     max_index = count_list.index(max(count_list))
     print(unique[max_index])
+
+"""
+counter = dict()
+
+data = input()
+
+#전형적인 카운터(Counter) 구현
+for x in data:
+    if x not in counter:
+        counter[x]=1
+    else:
+        counter[x] += 1
+
+#딕셔너리 순회
+#for (key, value) in counter.items():
+#    print(key, value)
+
+max_cnt = 0
+max_char = 'a'
+for (key, value) in counter.items():
+    if value > max_cnt:
+        max_char = key
+        if value == max_cnt:
+            cnt += 1
+
+cnt = 0
+for (key, value) in counter.items():
+    if value > max_cnt:
+        max_char = key
+        if value == max_cnt:
+            cnt += 1
+
+if cnt >= 2:
+    print('?')
+else:
+    print(max_char)
+
+"""
