6b22cf32580cc31fc65ec683fb79af87146e0188
Song <64202860+Song-Joo-Young@users.noreply.github.com>
2023-06-22 15:33:22 +0900

    Create 5622.py

---
+++ BOJ/5. 문자열/5622.py
@@ -0,0 +1,10 @@
+alpabet_list = ['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
+word = input()
+
+time = 0
+for unit in alpabet_list :  
+    for i in unit:       # alpabet 리스트 각 요소를 한글자씩 분리
+        for x in word :  # 입력받은 문자를 하나씩 분리
+            if i == x :  # 두 알파벳이 같으면
+                time += alpabet_list.index(unit) +3
+print(time)
