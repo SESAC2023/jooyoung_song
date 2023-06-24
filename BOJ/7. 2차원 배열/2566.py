868d3f0e68bdd74ebec712fd7a01cf9ea002ff45
Song <64202860+Song-Joo-Young@users.noreply.github.com>
2023-06-24 09:54:25 +0900

    Update 2566.py

--- BOJ/7. 2차원 배열/2566.py
+++ BOJ/7. 2차원 배열/2566.py
@@ -1,9 +1,8 @@
-# 틀린 코드
 array = [[]*9 for _ in range(9)]
 for i in range(9):
     array[i] = list(map(int,input().split()))
 
-max = 0
+max = -1 # 0으로 설정했었을 때 틀림
 max_x = -1
 max_y = -1
 for i in range(9):
