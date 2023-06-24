0e46ce43de8156f55fb65574581a22039a6302db
Song <64202860+Song-Joo-Young@users.noreply.github.com>
2023-06-24 14:18:31 +0900

    Update 2667.py

--- BOJ/31. 그래프와 순회/2667.py
+++ BOJ/31. 그래프와 순회/2667.py
@@ -3,6 +3,9 @@ from collections import deque
 input = sys.stdin.readline
 sys.setrecursionlimit(int(1e6))
 
+dx = {1, -1, 0, 0}
+dy = {0, 0, -1, 1}
+
 N = int(input())
 array = [[]*N for i in range(N)]
 visited = [[]*N for i in range(N)]
@@ -11,3 +14,13 @@ for i in range(N):
     array[i] = list(input().rstrip())
 
 print(array)
+
+def dfs(x, y):
+    visited[x][y] = True
+    for i in range(3):
+        nx = x+dx[i]
+        ny = y+dy[i]
+        if not visited[nx][ny]:
+            dfs(nx, ny)
+
+
