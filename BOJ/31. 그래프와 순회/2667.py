3c98e156835d0baec2aba2d80d3f06cde4ff7448
Song <64202860+Song-Joo-Young@users.noreply.github.com>
2023-06-24 15:15:04 +0900

    Update 2667.py

--- BOJ/31. 그래프와 순회/2667.py
+++ BOJ/31. 그래프와 순회/2667.py
@@ -13,46 +13,33 @@ visited = [[False]*N for i in range(N)]
 for i in range(N):
     array[i] = list(map(int, input().rstrip()))
 
-print(array)
-
-num = 3
-
-#테스트
-print()
-print(array[1][1])
-print(visited[1][1])
-print()
-#테스트
-
+num = 0
+answer_list = []
 
 def dfs(x, y):
     global num
+    global count
     array[x][y] = num
     visited[x][y] = True
+    count += 1
     for i in range(4):
-        if x+dx[i] < N and x+dx[i] > 0 and y+dy[i] < N and y+dy[i] > 0:
+        if x+dx[i] < N and x+dx[i] >= 0 and y+dy[i] < N and y+dy[i] >= 0:
            nx = x+dx[i]
            ny = y+dy[i]
           
-           # 테스트
-           print()
-           print(f"nx={nx}, ny = {ny}")
-           print()
-           #
-          
            if array[nx][ny] == 1 and not visited[nx][ny]:
-               # 테스트
-               print("DFS 체크")
                dfs(nx, ny)
 
-"""
 for i in range(N):
     for j in range(N):
-        visited = [[False]*N for i in range(N)]
-        dfs(i, j)
-        if pass
-        
-"""   
-dfs(0,1)
-print(array)
-print(visited)
+        if array[i][j] == 0:
+            continue
+        if not visited[i][j]:
+            num += 1
+            count = 0
+            dfs(i, j)
+            answer_list.append(count)
+          
+print(num)
+for i in answer_list:
+    print(i)
