e7cdb901fec2f1ee2da3c50f87722b9899c4dc42
Song <64202860+Song-Joo-Young@users.noreply.github.com>
2023-06-26 15:02:08 +0900

    Update 11005.py

--- BOJ/8. 일반 수학 1/11005.py
+++ BOJ/8. 일반 수학 1/11005.py
@@ -1,43 +1,19 @@
+import sys
+input = sys.stdin.readline
 N, M = list(map(int, input().split()))
 
 count = []
 temp = N
-cnt = 0
 
-for _ in range(5):
-    cnt = 0
-    
-    while(1):
-        if temp == 0:
-            break
-        temp = temp // M
-        cnt += 1
-        
-    count.append(cnt)
+while(1):
+    if temp < M:
+        count.append(temp)
+        break
+    count.append(temp % M)
+    temp = temp // M
 
-print(count)
-
-
-
-    
-
-"""
-
-for i in range(len(N)):
-    N[i] = N[i] - 55
-    if N[i] < 3: # 0 ~ 9를 그대로 나타내기 위한 조건
-        N[i] += 7
-
-temp = len(N) - 1
-
-for i in range(len(N)):
-    for _ in range(temp):
-        N[i] *= M
-    temp -= 1
-
-result = 0
-for j in range(len(N)):
-    result += N[j] 
-
-print(result)
-"""
+for i in reversed(range(len(count))):
+    if count[i] < 10:
+        print(count[i], end = '')
+    else:
+        print(chr(count[i]+55), end= '')
