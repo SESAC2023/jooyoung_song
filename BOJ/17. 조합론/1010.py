7bcc9947cdc35e8efdb3c9534ce74286724e1e1b
Song <64202860+Song-Joo-Young@users.noreply.github.com>
2023-06-27 22:10:57 +0900

    Update 1010.py

--- BOJ/17. 조합론/1010.py
+++ BOJ/17. 조합론/1010.py
@@ -8,13 +8,14 @@ for _ in range(T):
     n, m = map(int, input().split())
     site.append([n, m])
 
+# 답은 mCn, 이전 11050번 조합 계산 코드를 가져와서 변수 이름이 다를 수 있음
 for x in range(T):
     N, M = site[x]
 
     nf = 1
     nkf = 1
     kf = 1
-
+    
     for i in range(1,M+1):
         nf *= i
 
