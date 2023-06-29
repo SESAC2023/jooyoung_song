28100e431972e13a7d36d5215b6a455fe62667f8
Song <64202860+Song-Joo-Young@users.noreply.github.com>
2023-06-29 18:24:00 +0900

    Update 1707.py

--- BOJ/31. 그래프와 순회/1707.py
+++ BOJ/31. 그래프와 순회/1707.py
@@ -3,6 +3,7 @@ input = sys.stdin.readline
 from collections import deque
 
 k = int(input())
+result = []
 
 # 이분 그래프 = 그래프 정점의 집합을 둘로 나눴을 때 각 집합에 속한 정점끼리는 서로 인접하지 않도록 분할 가능한 그래프
 # = 그래프의 정점을 2개의 집합으로 구분할 때, 인접한 정점끼리는 다른 집합에 속한다.
@@ -27,12 +28,12 @@ def bfs(graph, start):
 
             elif visited[i] == 1:   # 방문을 한 경우. 1번 집합으로 설정했던 경우
                 if flag == 1:       # 인접 정점도 1번 집합인 경우
-                    print('NO')     # 이분 그래프가 아니므로 No 프린트 후 True return
+                    result.append('NO')     # 이분 그래프가 아니므로 No 저장 후 True return
                     return True
             elif visited[i] == 2:   # 방문을 한 경우. 2번 집합으로 설정했던 경우
                 if flag == 2:       # 인접 정점도 2번 집합인 경우
-                    print('NO')     # 이분 그래프가 
-                    return True    # 아니므로 No 프린트 후 True return
+                    result.append('NO')      # 이분 그래프가 
+                    return True    # 아니므로 No 저장 후 True return
     
     return False # while 문에서 return 이 안걸리면 False 반환
 
@@ -51,9 +52,12 @@ for _ in range(k):
 
     # 모든 정점에 대해 BFS
     for i in range(1, num_V + 1):
-        if bfs(graph, k):
+        if bfs(graph, i):
             print_check = 1
             break
 
     if print_check == 0:
-        print("YES")
+        result.append('YES')
+
+for answer in result:
+    print(answer)
