45ae69d1f58c4b384253792eab2396a3810e9fc7
Song <64202860+Song-Joo-Young@users.noreply.github.com>
2023-07-04 17:59:19 +0900

    Create 15650.py

---
+++ BOJ/22. 백트래킹/15650.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(int(1e6))
+
+N, M = map(int, input().split())
+answer = []
+
+def backtracking(start):
+    # M개의 원소를 가진 경우 출력 후 종료
+    if len(answer) == M:
+        print(' '.join(map(str, answer)))
+        return
+
+    for i in range(start, N+1):
+        if i not in answer:
+            answer.append(i) 
+            backtracking(i+1)
+            answer.pop()
+          
+backtracking(1)
